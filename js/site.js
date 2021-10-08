134bbe7cd06ffb397f886324bb1ce0f8b8e46e5a
a-santiago <santiago.62596@gmail.com>
2021-10-08 10:04:32 -0400

    commit_001
    broken Button

--- js/site.js
+++ js/site.js
@@ -1,9 +1,85 @@
+let events = [{
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017"
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018"
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019"
+    }
+]
 
-//Displays a message to the User
-function displayMessage() {
-    let msg = "";
-    msg =document.getElementById("message").value;
-    Swal.fire(msg);
+
+// Build a Dropdown of Distinct Cities
+function buildDropDown() {
+    //event DD is the Drop down items to select on HTML
+    let eventDD = document.getElementById("eventDropDown");
+    //clear out the drop down
+    eventDD.innerHTML = "";
+    // get the template
+    let ddTemplate = document.getElementById("cityDD-template");
+
+    let curEvents = events;
     
-}
+    let distinctEvents = [...new Set(curEvents.map((event) => event.city))];
 
+    let ddItemNode = document.importNode(ddTemplate.content, true);
+    let ddItem = ddItemNode.querySelector("a");
+    ddItem.setAttribute("data-string", "All");
+    ddItem.textContent = "All";
+    eventDD.appendChild(ddItem);
+}
